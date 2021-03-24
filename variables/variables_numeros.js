99595e46bc287214e3bbb948c572a46d22261cd0
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-03-24 19:25:50 +0100

    Numbers

---
+++ variables/variables_numeros.js
@@ -0,0 +1,21 @@
+var edad = 37;
+
+// Incremento
+
+edad +=1;
+edad++;
+console.log(edad);
+
+// Decimales
+
+var div = 1/3;
+divRound = Math.round(div);
+console.log(divRound);
+div = div.toFixed(2);
+console.log(div);
+
+var pizza = 8;
+var persona = 2;
+
+var cantidadDePorcionesPorPersona = pizza / persona;
+console.log(cantidadDePorcionesPorPersona);
