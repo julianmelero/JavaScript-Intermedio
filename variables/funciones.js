c952ffce5968b4650b39fa87346a00421cd3d7bc
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-03-26 15:41:58 +0100

    Basic functions

---
+++ variables/funciones.js
@@ -0,0 +1,15 @@
+var nombre = "Julián", edad = "37";
+
+function GetName(n,e) {
+    console.log(`${n} tiene ${e}`);
+}
+
+
+const nombreYEdad = (n, e) => {
+    console.log(`${n} tiene ${e}`);
+};
+
+
+
+nombreYEdad(nombre,edad);
+GetName(nombre, edad);
